bb181c606a9290af2f7ad47a1d3863b887225909
Asmaa Abubakr <asmaaabubakr04@gmail.com>
2026-01-25 14:43:31 +0200

    update4

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-orange-500" />
-                <span className="text-gray-300">Aswan, Egypt</span>
+                <span className="text-gray-300">Aswan,Egypt</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-orange-500" />
